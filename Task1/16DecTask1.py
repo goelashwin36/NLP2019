c602cafe434764b7c102464bf79bdaea206fa212
Ashwin Goel <goelashwin36@gmail.com>
2019-12-06 16:56:20 +0530

    Task1 added

---
+++ Task1/16DecTask1.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Represent a piece of text
+randomString="Ashwin Goel"
+
+
+# In[2]:
+
+
+# Taking text input and representing it as a variable
+randomInput = input("Enter some text: ")
+print(randomInput, end="\n\n")
+
+
+# In[3]:
+
+
+# Represent a paragraph on Shakespeare and print it
+shakespeare = '''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an
+English poet, playwright, and actor, widely regarded as the greatest writer in the
+English language and the world's greatest dramatist.[2][3][4] He is often
+called England's national poet and the "Bard of Avon" (or simply "the Bard").[5][b]
+His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets,
+two long narrative poems, and a few other verses, some of uncertain authorship.
+His plays have been translated into every major living language and are performed
+more often than those of any other playwright.[7]'''
+print(shakespeare, end="\n\n")
+
+
+# In[4]:
+
+
+hindi = '''विलियम शेक्सपियर (बपतिस्मा। 26 अप्रैल 1564 - 23 अप्रैल 1616) [a] एक था
+अंग्रेजी कवि, नाटककार और अभिनेता, व्यापक रूप से सबसे बड़े लेखक के रूप में माने जाते हैं
+अंग्रेजी भाषा और दुनिया का सबसे बड़ा नाटककार। [२] [३] [४] वह अक्सर होता है
+इंग्लैंड के राष्ट्रीय कवि और "बार्ड ऑफ एवन" (या बस "बार्ड") कहा जाता है। [५] [बी]
+उनके कार्यों में सहयोग शामिल है, जिसमें कुछ 39 नाटक शामिल हैं, [c] 154 सॉनेट्स,
+दो लंबी कथा कविताएँ, और कुछ अन्य छंद, कुछ अनिश्चित लेखक।
+उनके नाटकों का हर प्रमुख भाषा में अनुवाद किया गया है और उनका प्रदर्शन किया गया है
+किसी भी अन्य नाटककार की तुलना में अधिक बार।'''
+print(hindi, end="\n\n")
+
+
+
+
